67090474dc242585ddd799afd803fba1887c6592
prakharpradhan <52103939+prakharpradhan@users.noreply.github.com>
2019-07-20 16:49:04 +0530

    Add files via upload

--- pos/src/main/java/com/cg/pos/dao/PizzaDaoImpl.java
+++ pos/src/main/java/com/cg/pos/dao/PizzaDaoImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
@@ -12,9 +11,7 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 import com.cg.pos.dto.PizzaDetails;
-import com.cg.pos.dto.StoreDetails;
 import com.cg.pos.exceptions.PizzaException;
-import com.cg.pos.utility.ExceptionMessage;
 
 /**
  * Dao class to fetch PIZZA DETAILS according to his storeid
@@ -33,19 +30,12 @@ public class PizzaDaoImpl implements PizzaDao {
 	@Override
 	public List<PizzaDetails> getPizzaDetails(String store) throws PizzaException {
 		List<PizzaDetails> pizzaList = new ArrayList<PizzaDetails>();
-		StoreDetails details = new StoreDetails();
-		TypedQuery<StoreDetails> query = manager
-				.createQuery("select s from StoreDetails s  where s.storeName=:storeName", StoreDetails.class);
-		query.setParameter("storeName", store);
-		try {
-			details = query.getSingleResult();
-		} catch (NoResultException e) {
-			throw new PizzaException(ExceptionMessage.MESSAGE4);
-		}
 
-		TypedQuery<PizzaDetails> q = manager.createQuery("select p from PizzaDetails p  where p.storeID=:storeId",
+		TypedQuery<PizzaDetails> q = manager.createQuery(
+				"select p from PizzaDetails p inner join  StoreDetails s on p.storeID=s.storeId and  s.storeName=:storeName",
 				PizzaDetails.class);
-		q.setParameter("storeId", details.getStoreId());
+		q.setParameter("storeName", store);
+
 		pizzaList = q.getResultList();
 		return pizzaList;
 	}
